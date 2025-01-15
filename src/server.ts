2cb35de92359e8ecfb36e80b3f9a464e43ceefae
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-01-15 08:21:47 -0300

    feat: Servidor inicial

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("this is server.ts asd");
+import * as http from "http";
+
+const server = http.createServer(
+  (req: http.IncomingMessage, res: http.ServerResponse) => {}
+);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`🔥 Server running on port ${port} 🔥`);
+});
