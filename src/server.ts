7ed164d3ec861cb937be619931358f886bd2d396
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-01-28 07:37:14 -0300

    feat: implementado camada de controller e service

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import * as http from "http";
+import { getListEpisodes } from "./controllers/podcasts-controller";
 
 const server = http.createServer(
-  (req: http.IncomingMessage, res: http.ServerResponse) => {}
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if (req.method == "GET") {
+      await getListEpisodes(req, res);
+    }
+  }
 );
-
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log(`🔥 Server running on port ${port} 🔥`);
+  console.log(`🔥 Server running on port http://localhost:${port} 🔥`);
 });
---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,8 @@
+export const serviceListEpisodes = async () => {
+  const data = [
+    {
+      name: "Danilo Augusto service",
+    },
+  ];
+  return data;
+};
