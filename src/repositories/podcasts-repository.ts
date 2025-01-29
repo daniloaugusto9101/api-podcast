9e3e573671c89d4ffd86c85070188a2ad2becc5a
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-01-29 07:06:10 -0300

    feat: Ajuste camada repository

--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -4,8 +4,17 @@ import { Podcast } from "../models/podcast-model";
 
 const pathData = path.join(__dirname, "../repositories/podcasts.json");
 
-export const repositoryPodcast = async (): Promise<Podcast[]> => {
+export const repositoryPodcast = async (
+  podcastName?: string
+): Promise<Podcast[]> => {
   const rawData = fs.readFileSync(pathData, "utf-8");
-  const jsonFile = JSON.parse(rawData);
+  let jsonFile = JSON.parse(rawData);
+
+  if (podcastName) {
+    jsonFile = jsonFile.filter(
+      (podcast: Podcast) => podcast.name === podcastName
+    );
+  }
+
   return jsonFile;
 };
