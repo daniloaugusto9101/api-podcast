5b421b83401aa4de33c488432c20e37ae6596081
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-01-29 08:02:08 -0300

    feat: refatoração

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,6 +1,8 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { serviceListEpisodes } from "../services/list-episodes-service";
 import { serviceFilterEpisodes } from "../services/filter-episodes-service";
+import { StatusCode } from "../utils/status-code";
+import { ContentType } from "../utils/content-type";
 
 export const getListEpisodes = async (
   req: IncomingMessage,
@@ -8,7 +10,7 @@ export const getListEpisodes = async (
 ) => {
   const content = await serviceListEpisodes();
 
-  res.writeHead(200, { "Content-Type": "application/json" });
+  res.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
   res.end(JSON.stringify(content));
 };
 
@@ -16,10 +18,8 @@ export const getFilterEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
-  const queryString = req.url?.split("?p=")[1] || "";
+  const content = await serviceFilterEpisodes(req.url);
 
-  const content = await serviceFilterEpisodes(queryString);
-
-  res.writeHead(200, { "Content-Type": "application/json" });
+  res.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
   res.end(JSON.stringify(content));
 };
--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import {
   getFilterEpisodes,
   getListEpisodes,
 } from "./controllers/podcasts-controller";
+import { Routes } from "./routes/routes";
+import { HttpMethod } from "./utils/http-methods";
 
 const server = http.createServer(
   async (req: http.IncomingMessage, res: http.ServerResponse) => {
     const [baseUrl, queryString] = req.url?.split("?") ?? ["", ""];
 
-    if (req.method == "GET" && baseUrl === "/api/list") {
+    if (req.method == HttpMethod.GET && baseUrl === Routes.LIST) {
       await getListEpisodes(req, res);
     }
-    if (req.method == "GET" && baseUrl === "/api/episode") {
+    if (req.method == HttpMethod.GET && baseUrl === Routes.EPISODE) {
       await getFilterEpisodes(req, res);
     }
   }
